5584ede61674403de129650fc2c69f304adbcac8
Chris Hsu <chrishsu@ChrisdeMacBook-Pro.local>
2024-09-08 13:13:36 +0800

    Initial commit

---
+++ content/keyboard-review.md
@@ -0,0 +1,11 @@
+Title: 我的第一篇文章
+Date: 2010-12-03 10:20
+Category: Review
+
+Following is a review of my favorite mechanical keyboard.
+
+![SD 圖片](/images/image2.jpg "這是圖片標題")
+
+這是我撰寫的另一段
+
+這又是另外一段
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+AUTHOR = 'Chris Hsu'
+SITENAME = 'housearch'
+SITEURL = 'https://chrisincite.github.io'
+
+PATH = 'content'
+OUTPUT_PATH = 'docs/'
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = 'zh'
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
